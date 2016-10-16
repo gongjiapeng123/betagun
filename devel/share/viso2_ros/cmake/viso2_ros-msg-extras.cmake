348f7c65ba4df927a7e053203017fa8aa90cf9fb
liubiggun <372252659@qq.com>
2016-10-16 08:29:16 +0000

    配置好viso2，添加jy901

---
+++ devel/share/viso2_ros/cmake/viso2_ros-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(viso2_ros_MESSAGE_FILES "/root/workspace/betagun/src/viso2_ros/msg/VisoInfo.msg")
+set(viso2_ros_SERVICE_FILES "")
---
+++ devel/share/viso2_ros/cmake/viso2_ros-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(viso2_ros_MSG_INCLUDE_DIRS "/root/workspace/betagun/src/viso2_ros/msg")
+set(viso2_ros_MSG_DEPENDENCIES std_msgs)
